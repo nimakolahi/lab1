ad26ff499119dddb9b4be2b6cfd25f21ba9669b0
NIMA KOLAHIMAHMOUDI <87775939+nimakolahi@users.noreply.github.com>
2021-11-22 02:39:37 +0330

    lab1

--- lab1.2/syn/switching_activity.tcl
+++
@@ -1,6 +0,0 @@
-ungroup -all -flatten
-change_names -hierarchy -rules verilog
-write_sdf ../netlist/FIR.sdf
-write -f verilog -hierarchy -output ../netlist/FIR.v
-write_sdc ../netlist/FIR.sdc
-exit
--- lab1.3/syn/switching_activity_file_generator.tcl
+++
@@ -1,6 +0,0 @@
-ungroup -all -flatten
-change_names -hierarchy -rules verilog
-write_sdf ../netlist/FIR_unfolded.sdf
-write -f verilog -hierarchy -output ../netlist/FIR_unfolded.v
-write_sdc ../netlist/FIR_unfolded.sdc
-exit
